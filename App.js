5abdbefe01b932299c818bf6b11d9c4f910a15f6
konchanSS <b1447864@planet.kanazawa-it.ac.jp>
2018-02-02 19:46:16 +0900

    Height and Width lesson 1

--- App.js
+++ App.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { AppRegistry, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class FixedDimensionsBasics extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View >
+        <View style={{width: 50, height:  50, backgroundColor: 'powderblue'}}>
+          <Text>powderblue</Text>
+        </View>
+        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+AppRegistry.registerComponent('react-native-tutorial', () => FixedDimensionsBasics);
